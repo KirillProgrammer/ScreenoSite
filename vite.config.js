ea6de43ddc3ebab7e9fc4c561aaf532704c9f0c8
Kirill Karpov <kirillkarpovprogramm@gmail.com>
2022-03-27 13:01:45 +0300

    added next slides and adaptive

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,19 @@
 import {defineConfig} from 'vite';
+import autoprefixer from 'autoprefixer'
+
 export default defineConfig({
-    
+    root: 'src',
+    server: {
+        open: '/'
+    },
+    build: {
+        outDir: '../dist'
+    },
+    css: {
+        postcss:{
+            plugins:[
+              autoprefixer
+            ]
+        }
+    }
 });
